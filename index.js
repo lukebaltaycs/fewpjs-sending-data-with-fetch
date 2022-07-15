5e16bad761d24a4f35e619102f43ace733914f0c
Luke Baltay <luke.baltay@yale.edu>
2022-07-15 15:50:37 -0400

    commit tree d5bedc31b03f566d7f0635131bde8b7efe6e51b3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+    function submitData(name, email) {
+        const configObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({name: name, email: email})
+          };
+          
+        //   return fetch("http://localhost:3000/users", configObj)
+        //     .then(function(response) {
+        //       return response.json();
+        //     })
+            // .then(function(object) {
+            //     document.body.innerHTML = `<p>${object.body.id}</p>`
+            // //   console.log(object);
+            // })
+            // .catch((error) => {
+            //     document.body.innerHTML += `<p style = 'color: red'>${error.message}</p>`
+            // });
+    }
